ed9dca16bd232e774fc04a34df92cb0c94151993
Luiz Henrique Andrade <luizhenrique.andrade@corp.globo.com>
2019-02-05 17:44:48 -0200

    Add router settings

--- main.go
+++ main.go
@@ -1,2 +1,16 @@
 package main
 
+import(
+	"github.com/luizhenriqueandrade/go-project/api"
+	"log"
+	"net/http"
+)
+
+func main(){
+	addr := "127.0.0.1:8081"
+	router := http.NewServeMux()
+	router.Handle("/", &api.HomepageHandler{})
+	router.Handle("/new", &api.MovieHandler{})
+	log.Printf("Running web server on: http://%s\n", addr)
+	log.Fatal(http.ListenAndServe(addr, router))
+}
